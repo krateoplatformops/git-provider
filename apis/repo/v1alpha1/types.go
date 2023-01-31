8c264e5aa02ccedf510da0142be79b9328b87208
Luca Sepe <luca.sepe@gmail.com>
2023-01-31 09:55:55 +0100

    first commit

---
+++ apis/repo/v1alpha1/types.go
@@ -0,0 +1,93 @@
+package v1alpha1
+
+import (
+	commonv1 "github.com/krateoplatformops/provider-runtime/apis/common/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// RepoCredentials required to authenticate.
+type RepoCredentials struct {
+	// Credentials required to authenticate ReST API git server.
+	Credentials *commonv1.CredentialSelectors `json:"credentials"`
+
+	// AuthMethod defines the authentication mode. One of 'basic' or 'bearer'
+	// +optional
+	AuthMethod *string `json:"authMethod,omitempty"`
+}
+
+type RepoOpts struct {
+	// Url: the repository URL.
+	// +immutable
+	Url string `json:"url"`
+
+	// Path: name of the folder in the git repository
+	// to copy from (or to).
+	// +optional
+	// +immutable
+	Path *string `json:"path,omitempty"`
+}
+
+// A RepoSpec defines the desired state of a Repo.
+type RepoSpec struct {
+	commonv1.ManagedSpec `json:",inline"`
+
+	// FromRepo: repo origin to copy from
+	// +immutable
+	FromRepo RepoOpts `json:"fromRepo"`
+
+	// ToRepo: repo destination to copy to
+	// +immutable
+	ToRepo RepoOpts `json:"toRepo"`
+
+	// ConfigMapKeyRef: holds template values
+	// +optional
+	ConfigMapKeyRef *commonv1.ConfigMapKeySelector `json:"configMapKeyRef,omitempty"`
+
+	// DeploymentServiceUrl: the baseUrl for the Deployment service.
+	// +immutable
+	DeploymentServiceUrl string `json:"deploymentServiceUrl"`
+
+	// FromRepoCredentials required to authenticate ReST API git server.
+	FromRepoCredentials *RepoCredentials `json:"fromRepoCredentials,omitempty"`
+
+	// ToCredentials required to authenticate ReST API git server.
+	ToRepoCredentials *RepoCredentials `json:"toRepoCredentials,omitempty"`
+
+	// Insecure is useful with hand made SSL certs (default: false)
+	// +optional
+	Insecure *bool `json:"insecure,omitempty"`
+}
+
+// A RepoStatus represents the observed state of a Repo.
+type RepoStatus struct {
+	commonv1.ManagedStatus `json:",inline"`
+
+	// DeploymentId: correlation identifier with UI
+	DeploymentId *string `json:"deploymentId,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// A Repo is a managed resource that represents a Krateo Git Repository
+// +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
+// +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
+// +kubebuilder:printcolumn:name="DEPLOYMENT_ID",type="string",JSONPath=".status.atProvider.deploymentId"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,krateo,git}
+type Repo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RepoSpec   `json:"spec"`
+	Status RepoStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// RepoList contains a list of Repo.
+type RepoList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Repo `json:"items"`
+}

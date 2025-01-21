f33e8dc0be73a7007fa438abda5e99eb1573a9a0
Matteo Gastaldello <matteo.gastaldellomiotto@gmail.com>
2025-01-21 12:40:30 +0100

    chore: deps update to solve vulnerabilities - library cleaning

--- internal/clients/git/fakeRepo.go
+++ internal/clients/git/fakeRepo.go
@@ -3,14 +3,14 @@ package git
 import (
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
-	fixtures "github.com/go-git/go-git-fixtures/v4"
+	fixtures "github.com/go-git/go-git-fixtures/v5"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/cache"
 	"github.com/go-git/go-git/v5/storage/filesystem"
 )
 
 type BaseSuite struct {
-	fixtures.Suite
+	fixtures.Fixture
 	Repository *git.Repository
 	cache      map[string]*git.Repository
 }
